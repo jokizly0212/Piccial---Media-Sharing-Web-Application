5c3665eab9187ca98655947e304538b7055c5a31
jokizly0212 <doanminhphat0212@gmail.com>
2018-11-13 14:23:01 +0200

    First commit, uploaded the front-page, made it responsive

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+const get_started_button = document.querySelector('.get-start-button');
+const logo = document.querySelector('.logo');
+const form = document.querySelector('form');
+
+get_started_button.addEventListener('click', () => {
+    console.log('yes');
+    get_started_button.classList.add('slide-down-button');
+    logo.classList.add('logo-zoom-out');
+    form.classList.add('slide-up-form');
+});
+
